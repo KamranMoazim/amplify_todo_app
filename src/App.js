50f811866e1508f2083b8a596aa27aea202dbfc2
KamranMoazim <kamrannaseer765@gmail.com>
2023-01-02 23:35:50 +0500

    list todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import { API } from "aws-amplify";
+
+import * as queries from "./graphql/queries"
+import * as mutations from "./graphql/mutations"
 import './App.css';
 
+
 function App() {
+
+  const fetchTodos = async () => {
+
+    const response = await API.graphql({
+      query: queries.listTodos
+    })
+
+    console.log("fetching todos")
+    console.log(response)
+  }
+
+
+  const createTodo = async () => {
+
+    let myTodo = {
+      name: "todo 1",
+      description: "todo 1 description",
+    }
+
+    const response = await API.graphql({
+      query: mutations.createTodo,
+      variables: {
+        input: myTodo
+      }
+    })
+    console.log("creating todo")
+    console.log(response)
+  }
+  
+
+
   return (
     <div className="App">
       <h1>
         Hello World
       </h1>
+
+      <button onClick={fetchTodos}> Fetch Todos </button>
+      <button onClick={createTodo}> Create Todo </button>
     </div>
   );
 }
